d468ef8b2779da528a366d00eb0e1ff7d6ccfd22
kamrul25 <kamrul.coder01@gmail.com>
2023-06-13 22:27:35 +0600

    add all users page

--- src/layoutes/Dashboard.jsx
+++ src/layoutes/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
 
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <div className="drawer-content flex flex-col ">
           {/* Page content here */}
           <Outlet></Outlet>
           <label
--- src/pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -6,18 +6,59 @@ const ManageUsers = () => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     axios(`https://sports-server-two.vercel.app/users`).then((data) => {
+      console.log(data.data);
       setUsers(data.data);
     });
   }, []);
   return (
-    <div>
+    <div className="overflow-x-auto">
       <Helmet>
         <title>Sports | All Users</title>
       </Helmet>
-      <div>
-        {users.length}
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolorem consequuntur, expedita perferendis beatae quaerat nihil repellendus placeat. Commodi voluptatibus hic eum, velit cumque assumenda. Facere nobis soluta autem, tempore corporis quidem labore veniam doloremque ullam, iure eius ut? Eos error nulla hic pariatur ut eius ea sint ex aperiam debitis vero illum, eaque inventore laboriosam, similique dignissimos labore magni rerum. Dolorum ducimus id doloremque dolor consequatur at nobis nisi omnis tempora ipsum laudantium quidem vero quibusdam libero error facere voluptatibus aliquid, architecto perspiciatis sunt debitis delectus inventore. Corporis placeat error quos dolore facere tempora excepturi dignissimos consequatur nihil magni minima molestiae vel repellat quo impedit, distinctio ab similique delectus quod sunt. Hic fuga quam animi harum tempore blanditiis odio id? Aperiam impedit dolor, quo neque veniam sunt, eaque numquam tempora rem soluta aspernatur deserunt maiores quae quisquam recusandae distinctio, iure libero incidunt hic debitis voluptatem odit pariatur? Sit, veniam?</p>
-      </div>
+      <div></div>
+
+      <table className="table">
+        {/* head */}
+        <thead className="text-xl">
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user._id}>
+              <th>{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td className="space-x-2">
+                {user.role === "admin" ? (
+                  <button className="btn btn-success" disabled>
+                    Admin
+                  </button>
+                ) : (
+                  <button className="btn btn-success" >
+                    Admin
+                  </button>               
+                )}
+                {user.role === "instructor" ? (
+                  <button className="btn btn-success" disabled>
+                    Instructor
+                  </button>
+                ) : (
+                  <button className="btn btn-success" >
+                    Instructor
+                  </button>               
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
